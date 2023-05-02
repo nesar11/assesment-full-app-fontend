63e85781931d3fd076c8265bd1c0285ce24830d3
User <you@example.com>
2023-05-02 09:25:00 +0800

    update user

--- src/pages/UpdateUser.jsx
+++ src/pages/UpdateUser.jsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom';
 
 function UpdateUser() {
   const { id } = useParams();
-  const [value, setValues] = useState({
+  const [values, setValues] = useState({
     id: id,
     username: '',
     email: '',
@@ -17,23 +17,53 @@ function UpdateUser() {
           Authorization: `Bearer ${token}`, // set token in header
         },
       })
-      .then((res) => console.log(res))
-      .catch();
+      .then((res) => {
+        setValues({
+          ...values,
+          username: res.data.username,
+          email: res.data.email,
+        });
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   const token = localStorage.getItem('token');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .put('http://localhost:8000/users/update/' + id, values, {
+        headers: {
+          Authorization: `Bearer ${token}`, // set token in header
+        },
+      })
+      .then((res) => {
+        window.location.href = '/users';
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div>
       <h2>User Update</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Name:
-          <input type="text" name="username" />
+          <input
+            type="text"
+            name="username"
+            placeholder="User Name"
+            value={values.username}
+            onChange={(e) => setValues({ ...values, username: e.target.value })}
+          />
         </label>
         <label>
           Email:
-          <input type="email" name="email" />
+          <input
+            type="email"
+            name="email"
+            value={values.email}
+            onChange={(e) => setValues({ ...values, email: e.target.value })}
+          />
         </label>
         <button type="submit">Update</button>
       </form>
